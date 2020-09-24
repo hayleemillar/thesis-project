b0b9f2717d1df76df32a45fce0a0d04df9a6d0d4
haylee <hayleemmillar@gmail.com>
2020-09-24 14:56:01 -0400

    add security

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,10 +9,42 @@ use Symfony\Component\HttpFoundation\Response;
 class IndexController extends AbstractController
 {
      /**
-      * @Route("/")
+      * @Route("/", name="index")
       */
     public function index()
     {
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/text", name="text")
+     */
+    public function text()
+    {
+        return $this->render('text.html.twig');
+    }
+
+    /**
+     * @Route("/audio", name="audio")
+     */
+    public function audio()
+    {
+        return $this->render('audio.html.twig');
+    }
+
+    /**
+     * @Route("/video", name="video")
+     */
+    public function video()
+    {
+        return $this->render('video.html.twig');
     }
 }
